341e129634f7db13adebf78fc5c41efec0f5fe79
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-06-05 10:12:23 +0200

    made dalexception and userdao

---
+++ 15_CDIO_final/src/dao/SerUserDAO.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.util.List;
+
+import dto.UserDTO;
+import interfaces.IUserDAO;
+
+public class SerUserDAO implements IUserDAO{
+
+	@Override
+	public UserDTO getUser(int oprId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserDTO> getUserList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createUser(UserDTO opr){
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUser(UserDTO opr){
+		// TODO Auto-generated method stub
+		
+	}
+
+}
