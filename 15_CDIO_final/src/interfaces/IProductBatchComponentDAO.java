0d3cd54d66baabf8b416c7e67e5cabb0c356f958
s147300 <s147300@student.dtu.dk>
2017-06-06 11:52:10 +0200

    ProductBatchCompDAO completed.

--- 15_CDIO_final/src/interfaces/IProductBatchComponentDAO.java
+++ 15_CDIO_final/src/interfaces/IProductBatchComponentDAO.java
@@ -6,10 +6,41 @@ import dto.ProductBatchComponentDTO;
 import exceptions.DALException;
 
 public interface IProductBatchComponentDAO {
-	    ProductBatchComponentDTO getProduktBatchComp(int pbId, int ingrbatchId) throws DALException;
-	    List<ProductBatchComponentDTO> getProductBatchComponentList(int pbId) throws DALException;
-	    List<ProductBatchComponentDTO> getProductBatchComponentList() throws DALException;
-	    void createProductBatchComponent(ProductBatchComponentDTO productbatchcomponent) throws DALException;
-	    void updateProductBatchComponent(ProductBatchComponentDTO productbatchComponent) throws DALException;
+	/**
+	 * 
+	 * @param pbId (productbatch id)
+	 * @param ingrbatchId (ingredientbatch id)
+	 * @return ProductBatchComponent
+	 * @throws DALException
+	 */
+	ProductBatchComponentDTO getProduktBatchComp(int pbId, int ingrbatchId) throws DALException;
+
+	/**
+	 * 
+	 * @param pbId (producbatch id)
+	 * @return List of productbatch with specified receptID
+	 * @throws DALException
+	 */
+	List<ProductBatchComponentDTO> getProductBatchComponentList(int pbId) throws DALException;
+	
+	/**
+	 * 
+	 * @return all ProductBatchComponents.
+	 * @throws DALException
+	 */
+	List<ProductBatchComponentDTO> getProductBatchComponentList() throws DALException;
+
+	/**
+	 * @param productbatchcomponent
+	 * @throws DALException
+	 */
+	void createProductBatchComponent(ProductBatchComponentDTO productbatchcomponent) throws DALException;
+	
+	/**
+	 * 
+	 * @param productbatchComponent
+	 * @throws DALException
+	 */
+	void updateProductBatchComponent(ProductBatchComponentDTO productbatchComponent) throws DALException;
 
 }
