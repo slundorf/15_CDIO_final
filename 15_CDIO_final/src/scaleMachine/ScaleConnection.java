0ffaa1097ec1bd09e82000fc153e511dea1bdd9d
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-06-07 17:52:50 +0200

    Scaler

--- 15_CDIO_final/src/scaleMachine/ScaleConnection.java
+++ 15_CDIO_final/src/scaleMachine/ScaleConnection.java
@@ -2,16 +2,10 @@ package scaleMachine;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import javax.servlet.ReadListener;
 
 public class ScaleConnection {
 
@@ -21,7 +15,7 @@ public class ScaleConnection {
 	Socket clientSocket;
 	PrintWriter outToServer;
 	BufferedReader inFromServer;
-	private SocketListener listener;
+//	private SocketListener listener; LAvet af hjælpelærer. Vent med at slet.
 	private BufferedReader bufferedReader;
 
 	public ScaleConnection(String ip) {
@@ -80,38 +74,56 @@ public class ScaleConnection {
 	}
 	
 	
+//	Skulle vi have brugt, men har ikke tid til at implementere det.
 	
-	private class SocketListener implements Runnable {
-		
-		private BufferedReader bufferedReader;
-		private ScaleProcedure scaleController;
-
-		public SocketListener(InputStream inputStream, ScaleProcedure scaleController) {
-			bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-			this.scaleController = scaleController;
-		}
-
-		@Override
-		public void run() {
-			while (true){
-				try {
-					String readLine = bufferedReader.readLine();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-			
-		}
-		
-	}
+//	private class SocketListener implements Runnable {
+//		
+//		private BufferedReader bufferedReader;
+//		private ScaleProcedure scaleController;
+//
+//		public SocketListener(InputStream inputStream, ScaleProcedure scaleController) {
+//			bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//			this.scaleController = scaleController;
+//		}
+//
+//		@Override
+//		public void run() {
+//			while (true){
+//				try {
+//					String readLine = bufferedReader.readLine();
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				
+//			}
+//			
+//		}
+//		
+//	}
 
 
 
 	public double tara(){
 		outToServer.write("T");
 		return 0;
+		
+	}
+// Hvad sker der hvis det nu ikke er et nummer? Så skal den spørger igen.
+	
+	public int getNumber(String string) throws IOException, InputException {
+		// TODO Auto-generated method stub
+		outToServer.write("RM20 8 " + string + "" );
+		outToServer.flush();
+		String readLine = readFromSocket();
+		if ("RM20 B".equals(readLine)){
+			String readLine2 = inFromServer.readLine();
+			//Parse return string 
+			return Integer.parseInt(readLine2.split("RM20 A")[1]);
+			
+		} else {
+			throw new InputException();
+		}
 		
 	}
 
