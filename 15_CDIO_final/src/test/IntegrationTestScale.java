70bae7833b4873e546c92aa414bc897b3db6c5db
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-06-11 16:54:34 +0200

    test started

---
+++ 15_CDIO_final/src/test/IntegrationTestScale.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author jonaslarsen
+ *
+ */
+public class IntegrationTestScale {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
